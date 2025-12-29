c3f505bf2e36c4a02f9c3823cfd1bc7b46ccdbfc
ikki yamanaka <ikki.yama@icloud.com>
2025-12-29 14:55:52 +0900

    initial commit

---
+++ ros2_kadai2/translator_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros2_kadai2.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2_kadai2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ikki',
+    maintainer_email='ikki.yama@icloud.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'translator_node = ros2_kadai2.translator_node:main'
+        ],
+    },
+)
