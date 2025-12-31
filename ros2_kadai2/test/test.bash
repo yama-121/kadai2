a060dbb3823502a3ec6587dcaee3805b326461d0
ikki yamanaka <ikki.yama@icloud.com>
2025-12-31 15:19:24 +0900

    add

--- ros2_kadai2/test/test.bash
+++ ros2_kadai2/test/test.bash
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-dir=~
-[ "$1" != "" ] && dir="$1"
-
 source /opt/ros/humble/setup.bash
-
 export ROS_DOMAIN_ID=0
 export ROS_LOCALHOST_ONLY=1
+
+dir=~
+[ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
 colcon build --packages-select ros2_kadai2
@@ -15,11 +14,14 @@ source install/setup.bash
 rm -f /tmp/morse.log
 
 timeout 60 ros2 launch ros2_kadai2 morse.launch.py > /tmp/morse.log &
-sleep 20
-ros2 topic pub /input_text std_msgs/String "data: 'HELLO'" --once -w 1
+
+sleep 15
+
+ros2 topic pub -r 10 /input_text std_msgs/String "data: 'HELLO'" --once-for 3
+
 sleep 5
 
-if grep -q "\.\.\.\. \. \.\-\.\. \.\-\.\. \-\-\-" /tmp/morse.log; then
+if grep -p "Published: \.\.\.\. \. \.\-\.\. \.\-\.\. \-\-\-" /tmp/morse.log; then
     echo "0"
     exit 0
 else
